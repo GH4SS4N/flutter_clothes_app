f88b5b816a3e63e9d26dfda4b342ac798d7f353d
Saud Bako <saud.bako@gmail.com>
2020-10-25 18:09:53 +0300

    edited Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+
+ 'package:flutter/material.dart';
 import 'package:flutter_clothes_app/Data/Order.dart';
 
 import 'Data/Customer.dart';
@@ -7,6 +8,7 @@ import 'Pages/SearchCustomer.dart';
 import 'Pages/OrdersHome.dart';
 
 void main() {
+  //TODO:make the login page and the checker(Ghassan)
   runApp(MyApp());
 }
 
@@ -39,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
     connectParse()
         // run code after connection
         .then(onConnection)
-        // TODO: notify the user of the error
+        // TODO: notify the user of the error(Ghassan)
         .catchError((error) =>
             print('Could not connect to Parse.\n' + error.toString()));
   }
@@ -56,86 +58,80 @@ class _MyHomePageState extends State<MyHomePage> {
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
-          drawer: Drawer(
-            child: Container(
-              color: Colors.black,
-              child: ListView(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      setState(() {
-                        current = SearchCustmer();
-                      });
+            drawer: Drawer(
+              child: Container(
+                color: Colors.black,
+                child: ListView(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        setState(() {
+                          current = SearchCustmer();
+                        });
 
-                      Navigator.pop(context);
-                    },
-                    focusColor: Colors.amber,
-                    contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    leading: Icon(
-                      Icons.search,
-                      color: Colors.white,
+                        Navigator.pop(context);
+                      },
+                      focusColor: Colors.amber,
+                      contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      leading: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      title: Text(
+                        'Search',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                    title: Text(
-                      'Search',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  ListTile(
-                    onTap: () {
-                      setState(() {
-                        current = OrdersHome();
-                      });
+                    ListTile(
+                      onTap: () {
+                        setState(() {
+                          current = OrdersHome();
+                        });
 
-                      Navigator.pop(context);
-                    },
-                    focusColor: Colors.amber,
-                    contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    leading: Icon(Icons.home, color: Colors.white),
-                    title: Text(
-                      'Home',
-                      style: TextStyle(color: Colors.white),
+                        Navigator.pop(context);
+                      },
+                      focusColor: Colors.amber,
+                      contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      leading: Icon(Icons.home, color: Colors.white),
+                      title: Text(
+                        'Home',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    focusColor: Colors.amber,
-                    contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    leading: Icon(Icons.subdirectory_arrow_right,
-                        color: Colors.white),
-                    title: Text(
-                      'Sign up a user',
-                      style: TextStyle(color: Colors.white),
+                    ListTile(
+                      focusColor: Colors.amber,
+                      contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      leading: Icon(Icons.subdirectory_arrow_right,
+                          color: Colors.white),
+                      title: Text(
+                        'Sign up a user',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    focusColor: Colors.amber,
-                    contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    leading: Icon(Icons.do_not_disturb_on, color: Colors.red),
-                    title: Text(
-                      'Logout',
-                      style: TextStyle(color: Colors.white),
+                    ListTile(
+                      focusColor: Colors.amber,
+                      contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      leading: Icon(Icons.do_not_disturb_on, color: Colors.red),
+                      title: Text(
+                        'Logout',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          appBar: AppBar(
-            flexibleSpace: Row(
-              children: [Icon(Icons.access_alarm)],
+            appBar: AppBar(
+              flexibleSpace: Row(
+                children: [Icon(Icons.access_alarm)],
+              ),
+              bottomOpacity: 0.9,
+              elevation: 14,
+              title: Text("Hamdan's"),
+              centerTitle: true,
+              backgroundColor: Colors.black,
             ),
-            bottomOpacity: 0.9,
-            elevation: 14,
-            title: Text("Hamdan's"),
-            centerTitle: true,
-            backgroundColor: Colors.black,
-          ),
-          // TODO: replace the loading icon with visualization
-          body: connected
-              ? current
-              : Center(
-                  child: Icon(Icons.loop),
-                ),
-        ),
+            body: current),
       ),
     );
   }
